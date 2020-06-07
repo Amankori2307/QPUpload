87718c0278c6241ec31e04030b8636ce2940df94
amankori2307 <amankori2307@gmail.com>
2020-06-07 11:29:25 +0530

    Uploads folder added

--- server/index.js
+++ server/index.js
@@ -41,5 +41,4 @@ const port = process.env.PORT || 5000;
 app.listen(port, (err) => {
     if(err) console.log(err);
     else console.log(`Listening to port ${port}`);
-    console.log(process.env.NODE_ENV);
 })
